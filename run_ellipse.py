c000f7b473b9831f76f21c28668d86cc8145934a
kadubarbosa <kadu.barbosa@gmail.com>
2016-12-15 12:51:26 -0200

    Changed the structure of the projec.

--- run_ellipse.py
+++ run_ellipse.py
@@ -7,10 +7,13 @@ Created on 07/12/2016
 
 """
 import os
+import shutil
 
 import numpy as np
 import pyfits as pf
-from pyraf import iraf
+from pyraf import iraf, iraffunctions
+import astropy.units as u
+from astropy.coordinates import SkyCoord
 from astropy.io.ascii import SExtractor
 from astropy.modeling.models import Ellipse2D
 import matplotlib.pyplot as plt
@@ -19,57 +22,82 @@ iraf.stsdas(_doprint=False)
 iraf.analysis(_doprint=False)
 iraf.isophote(_doprint=False)
 
-def run_ellipse():
-    wdir = os.path.join(os.getcwd(), "SFCG280")
+def run_ellipse_fixed_geom(group):
+    """ Run ellipse into a given group. """
+    wdir = os.path.join(os.getcwd(), group)
     os.chdir(wdir)
-    imgs = ["rband_SFCG280.wcs.fits"]
-    for img in imgs:
-        objid = img.split("_")[1].split(".")[0]
-        cat = img.replace(".wcs.fits", ".2.5.cat")
-        pars = np.loadtxt("{}_apers.dat".format(objid))
-        table = SExtractor().read(cat)
-        avoid = []
-        for i, obj in enumerate(table):
-            r = np.sqrt((pars[:,0] - obj["X_IMAGE"])**2 +
-                        (pars[:,1] - obj["Y_IMAGE"])**2)
-            if r.min() == 0.:
-                avoid.append(i)
-        # make_mask(img, cat, avoid_objs=avoid)
-        iraf.imcopy(input="mask.fits[type=mask]", output=img + ".pl")
-        for i,p in enumerate(pars):
-            x0, y0, a, b, pa = p
-            pa0 = pa + 90
-            pa0 = pa0 if pa0 <= 90. else pa0 - 180
+    rband = "rband_{}.wcs.fits".format(group)
+    rsub ="rband_{}.wcs.bcksub.fits".format(group)
+    catalog = "rband_{}.2.5.cat".format(group)
+    cat = SExtractor().read(catalog)
+    data = np.genfromtxt("{}_apers.dat".format(group), dtype=None)
+    names = []
+    idxs = []
+    for x0, y0, a, b, pa in data:
+        # Finding object in catalog
+        idx = np.argmin(np.sqrt((x0 - cat["X_IMAGE"])**2 +
+                                (y0 - cat["Y_IMAGE"])**2))
+        idxs.append(idx)
+        center = SkyCoord(ra=cat[idx]["ALPHA_SKY"] * u.degree,
+               dec=cat[idx]["DELTA_SKY"] * u.degree).to_string(style=u"hmsdms")
+        # Make identification of object
+        name = "".join([x.split(".")[0] for x in center.split()])
+        for char in "hdms. ":
+            name = name.replace(char, "")
+        names.append(name)
+        rundir = os.path.join(wdir, name)
+        if not os.path.exists(rundir):
+            os.mkdir(rundir)
+        os.chdir(rundir)
+        iraffunctions.chdir(rundir)
+        shutil.copy(os.path.join(wdir, rsub), ".")
+        shutil.copy(os.path.join(wdir, rband), ".")
+        make_mask(rband, cat, avoid_objs=[idx])
+        if not os.path.exists(rsub + ".pl"):
+            iraf.imcopy(input="mask.fits[type=mask]", output=rsub + ".pl")
+        pa0 = pa + 90
+        pa0 = pa0 if pa0 <= 90. else pa0 - 180
+        ellip = 1 - b / a
+        outtable = "fixed_geom.tab"
+        if not os.path.exists(outtable):
+            iraf.ellipse(input=rsub, output=outtable, x0=x0, y0=y0,
+                         ellip0=ellip, pa0=pa0, hcenter="yes", hellip="yes",
+                         hpa="yes", interactive="no")
+        bmodel = outtable.replace(".tab", "_bmodel.fits")
+        if not os.path.exists(bmodel):
+            iraf.bmodel(table=outtable, output=bmodel, parent=rsub)
+    os.chdir(wdir)
+    iraffunctions.chdir(wdir)
+    make_mask(rband, cat, avoid_objs=idxs)
+    mask = pf.getdata("mask.fits")
+    r = pf.getdata(rband)
+    sub = pf.getdata(rsub)
+    sky = r - sub
+    rms = np.sqrt(np.nanmean(sky**2))
+    bmodel = np.array([pf.getdata(os.path.join(wdir, gal,
+                                "fixed_geom_bmodel.fits")) for gal in names])
+    bmodel = bmodel.sum(axis=0)
+    resid = sub - bmodel
+    sigimg = resid / np.sqrt(r)
+    plt.imshow(np.ma.masked_where(mask!=0, sigimg), vmin=-3, vmax=3,
+               interpolation="none", origin="bottom")
+    plt.show(block=True)
+    h = pf.getheader(rband)
+    pf.writeto("bmodel.fits", bmodel, h, clobber=True)
+    pf.writeto("residuals.fits", resid, h, clobber=True)
+    pf.writeto("sigmaimg.fits", sigimg, h, clobber=True)
+    return
 
-            ellip = 1 - b / a
-            outtable = "{0}_{1:02d}.tab".format(objid, i+1)
-            if not os.path.exists(outtable):
-                iraf.ellipse(input=img, output=outtable, x0=x0, y0=y0,
-                             ellip0=ellip, pa0=pa0, hcenter="yes", hellip="yes",
-                             hpa="yes", interactive="no")
-            bmodel = outtable.replace(".tab", "_bmodel.fits")
-            if not os.path.exists(bmodel):
-                iraf.bmodel(table=outtable, output=bmodel, parent=img)
-        data = pf.getdata(img)
-        bmodel = np.array([pf.getdata(x) for x in os.listdir(".")
-                           if x.endswith("_bmodel.fits")])
-        bmodel = bmodel.sum(axis=0)
-        resid = data - bmodel
-        sigimg = resid / np.sqrt(data)
-        h = pf.getheader(img)
-        pf.writeto("bmodel.fits", bmodel, h, clobber=True)
-        pf.writeto("residuals.fits", resid, h, clobber=True)
-        pf.writeto("sigmaimg.fits", sigimg, h, clobber=True)
-
-def make_mask(im, cat, avoid_objs=None):
+def make_mask(im, cat, avoid_objs=None, redo=False):
     """ Produces a mask for a given image from a SExtractor catalog. """
-    table = SExtractor().read(cat)
+    if os.path.exists("mask.fits") and not redo:
+        return
     imdata = pf.getdata(im)
     h = pf.getheader(im)
     xis = [np.arange(x) for x in imdata.shape]
     xx, yy = np.meshgrid(*xis)
     mask = np.zeros_like(imdata)
-    for i, obj in enumerate(table):
+    for i, obj in enumerate(cat):
         if i in avoid_objs:
             continue
         x0, y0, a, b, theta, rkron= obj["X_IMAGE"], obj["Y_IMAGE"], \
@@ -80,6 +108,7 @@ def make_mask(im, cat, avoid_objs=None):
         mask += e(xx, yy)
     hdu = pf.PrimaryHDU(data=mask, header=h)
     hdu.writeto("mask.fits", clobber=True)
+    return
 
 
 
@@ -87,4 +116,4 @@ def make_mask(im, cat, avoid_objs=None):
 
 
 if __name__ == "__main__":
-    run_ellipse()
+    run_ellipse_fixed_geom("SFCG280")
